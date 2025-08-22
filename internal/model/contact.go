943c705557f2cc66bda0b6eb20951ba3662a2786
ajaynegi45 <ajaynegi3345@icloud.com>
2025-08-22 17:56:25 +0530

    fix: typo error

--- internal/model/contact.go
+++ internal/model/contact.go
@@ -2,7 +2,7 @@ package model
 
 type ContactForm struct {
 	Name           string `json:"name"`
-	Email          string `json:"email"`
+	Email          string `json:"service"`
 	Subject        string `json:"subject"`
 	Message        string `json:"message"`
 	ProductName    string `json:"product_name,omitempty"`
